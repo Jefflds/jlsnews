e94d3106384c6b40b43416bb1208320e8a18a80d
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-07-06 20:55:34 -0300

    Remoção de exemplo de react.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './HelloWorld';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWorld/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello, World
